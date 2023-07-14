d56f51b594e3f761685b499512f6c2d27abedb8d
Erikas Bykovskis <94749107+erikasby@users.noreply.github.com>
2023-07-14 06:55:53 +0000

    Unable to run via Codespaces, will check at home

--- 3-deploy-a-quarkus-application-to-azure-container-apps/todo/src/test/java/com/example/demos/TodoResourceIT.java
+++
@@ -1,8 +0,0 @@
-package com.example.demos;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class TodoResourceIT extends TodoResourceTest {
-    // Execute the same tests but in packaged mode.
-}
